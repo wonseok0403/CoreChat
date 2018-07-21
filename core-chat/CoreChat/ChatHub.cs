2621b72c0af01831133aae5948ac19c05a406059
wonseok <wonseok0403@naver.com>
2018-07-22 01:48:14 +0900

    first commit

---
+++ core-chat/CoreChat/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace CoreChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
